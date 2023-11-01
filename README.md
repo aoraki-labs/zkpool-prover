2527b315aa754e080f4b5d1674b04730e4e60591
zkrollupdev <zkpool_satoshi@aoraki-labs.io>
2023-11-01 14:28:30 +0800

    update README

--- README.md
+++ README.md
@@ -21,67 +21,15 @@ It is used to connect with ZKPool in order to obtain proving tasks and generate 
 
 Coming soon.
 
-## Run from the prebuilt binary
+### Build and Run
 
-### Download the prebuilt binary
-
-Visit https://github.com/aoraki-labs/zkpool-prover/releases and download the latest release or use wget command like below. 
-Please ensure that you select the appropriate tar file for your hardware and the correct release version.
+Note:just for some particular test 
 ```
-   wget https://github.com/aoraki-labs/zkpool-prover/releases/download/v1.0-taiko-a5/x86_64-unknown-linux-musl.tar.gz
-   tar -zxvf x86_64-unknown-linux-musl.tar.gz
+   cargo build --release
+   cd target/release/
+   wget https://storage.googleapis.com/zkevm-circuits-keys/kzg_bn254_22.srs -P ./
+   ./zkpool-prover
 ```
-
-### Download the running script
-
-Download the script run_prover.sh from https://github.com/aoraki-labs/zkpool-prover/tree/taiko-a5-testnet
-
-### Run
-
-Modify the ./run-prover.sh according to your own config.
-```
-ACCESS_KEY=123456789 #replace the parameter with what we get in zkpool.io
-DEVICE_ID=123456789 #replace the parameter with the id name you want to set
-```
-
-Run like this:
-```
-   chmod +x ./run-prover.sh
-   ./run-prover.sh
-```
-
-## Build from source and run
-
-### Preparation
-
-Download codes via `git clone`. Make sure you have installed rustup, cargo and Go.
-```
-   git clone https://github.com/aoraki-labs/zkpool-prover.git
-   cd zkpool-prover
-```
-
-
-### Build
-
-Run `cargo build --release` to build the binary.
-And then, 
-`cp ./target/release/zkpool-prover .`
-
-### Run
-
-Modify the ./run-prover.sh according to your own config.
-```
-ACCESS_KEY=123456789 #replace the parameter with what we get in zkpool.io
-DEVICE_ID=123456789 #replace the parameter with the id name you want to set
-```
-
-Run like this:
-```
-   chmod +x ./run-prover.sh
-   ./run-prover.sh
-```
-You can also see more detail in run-prover.sh and refer to the usage help (`./zkpool-prover --help`):
-
 
 ## License
 
--- src/main.rs
+++ src/main.rs
@@ -85,64 +85,63 @@ async fn main() {
     tracing::subscriber::set_global_default(subscriber).expect("unable to set global default subscriber");
 
     info!("*************TEST***********");
-
-    let task_handle = task::spawn(async move {
+    // loop{
+        let task_handle = task::spawn(async move {
+            let time_started = Instant::now();
+            let test = generate_proof
+            ("https://rpc.jolnir.taiko.xyz/".to_string(),
+            20865 as u64,
+            "94061Fd498291Ff1F1b8C0d1a94e2EDC2a0A2f9D".to_string(),
+            "cD5e2bebd3DfE46e4BF96aE2ac7B89B22cc6a982".to_string(),
+            "1000777700000000000000000000000000000007".to_string(),
+            "1000777700000000000000000000000000000001".to_string(),
+            "322e41c411a8223cce152999b30ee00b8f29dc5e62e02f43e0dc7a77aa862fa8".to_string(),
+            "c73622fae1fbc1d1d9e4a9b7bbdb6733595c1c98a2470ea59ca3b9fee9ba3894".to_string(),
+            "afcb03ea890fb2d5ba0042fcda321d8879687fb87a8d68b8ef4417dbc86754b0".to_string(),
+            "9cc94396d73d6c51d8185249a1bcc7c55c87b3d6b67ce72600cfc8448dadc007".to_string(),
+            "0000000000000000000000000000000000000000000000000000000000000000".to_string(),
+            1241987 as u64,
+            328517 as u64,
+            8000000, 
+            0,
+            120000,
+            )
+            .await.unwrap();
+        let time_gap =(Instant::now().duration_since(time_started).as_millis() as u32)/1000;
+        println!("testcase 20865:proof result is {},time consumed:{}s",format!("{}",test.proof),time_gap);
+    });
+    
+        info!("*************SLEEP***********");
+        time::sleep(time::Duration::from_secs(400)).await; //estimated time
+    
+        info!("*************cancel the above task***********");
+        task_handle.abort(); 
+        drop(task_handle);
+    
+        info!("*************compute another one task agin***********");
         let time_started = Instant::now();
         let test = generate_proof
-        ("https://rpc.jolnir.taiko.xyz/".to_string(),
-        20865 as u64,
-        "94061Fd498291Ff1F1b8C0d1a94e2EDC2a0A2f9D".to_string(),
-        "cD5e2bebd3DfE46e4BF96aE2ac7B89B22cc6a982".to_string(),
-        "1000777700000000000000000000000000000007".to_string(),
-        "1000777700000000000000000000000000000001".to_string(),
-        "322e41c411a8223cce152999b30ee00b8f29dc5e62e02f43e0dc7a77aa862fa8".to_string(),
-        "c73622fae1fbc1d1d9e4a9b7bbdb6733595c1c98a2470ea59ca3b9fee9ba3894".to_string(),
-        "afcb03ea890fb2d5ba0042fcda321d8879687fb87a8d68b8ef4417dbc86754b0".to_string(),
-        "9cc94396d73d6c51d8185249a1bcc7c55c87b3d6b67ce72600cfc8448dadc007".to_string(),
-        "0000000000000000000000000000000000000000000000000000000000000000".to_string(),
-        1241987 as u64,
-        328517 as u64,
-        8000000, 
-        0,
-        120000,
+            ("https://rpc.jolnir.taiko.xyz/".to_string(),
+            942261 as u64,
+            "2909Db987AA74120a15f743197c58bE1B8D5e83b".to_string(),
+            "cD5e2bebd3DfE46e4BF96aE2ac7B89B22cc6a982".to_string(),
+            "1000777700000000000000000000000000000007".to_string(),
+            "1000777700000000000000000000000000000001".to_string(),
+            "73656a6ac138308428f6bb3fc55f2180f38e396c951d0069629b876456a8d564".to_string(),
+            "f1f4d8c9deac229fcf7f62b9c4940648248564dbcdf7402e0b47cbcfafd38307".to_string(),
+            "cb7e54df5335fcab508c38ba3954835794368cd81c02d1ce1f345e50c36a1b02".to_string(),
+            "4a611f97b71b197b26ff47e1844f7db878afb5bee3d711b0c9d33c0aee6e986b".to_string(),
+            "0000000000000000000000000000000000000000000000000000000000000000".to_string(),
+            127844 as u64,
+            127844 as u64,
+            8000000, 
+            0,
+            120000,
         )
         .await.unwrap();
-    let time_gap =(Instant::now().duration_since(time_started).as_millis() as u32)/1000;
-    println!("testcase 20865:proof result is {},time consumed:{}s",format!("{}",test.proof),time_gap);
-});
-
-    info!("*************SLEEP***********");
-    time::sleep(time::Duration::from_secs(400)).await; //estimated time
-
-    info!("*************cancel the above task***********");
-    task_handle.abort(); 
-    drop(task_handle);
-
-    info!("*************compute another one task agin***********");
-    let time_started = Instant::now();
-    let test = generate_proof
-        ("https://rpc.jolnir.taiko.xyz/".to_string(),
-        942261 as u64,
-        "2909Db987AA74120a15f743197c58bE1B8D5e83b".to_string(),
-        "cD5e2bebd3DfE46e4BF96aE2ac7B89B22cc6a982".to_string(),
-        "1000777700000000000000000000000000000007".to_string(),
-        "1000777700000000000000000000000000000001".to_string(),
-        "73656a6ac138308428f6bb3fc55f2180f38e396c951d0069629b876456a8d564".to_string(),
-        "f1f4d8c9deac229fcf7f62b9c4940648248564dbcdf7402e0b47cbcfafd38307".to_string(),
-        "cb7e54df5335fcab508c38ba3954835794368cd81c02d1ce1f345e50c36a1b02".to_string(),
-        "4a611f97b71b197b26ff47e1844f7db878afb5bee3d711b0c9d33c0aee6e986b".to_string(),
-        "0000000000000000000000000000000000000000000000000000000000000000".to_string(),
-        127844 as u64,
-        127844 as u64,
-        8000000, 
-        0,
-        120000,
-    )
-    .await.unwrap();
-    let time_gap =(Instant::now().duration_since(time_started).as_millis() as u32)/1000;
-    println!("testcase 942261:proof result is {},time consumed:{}s",format!("{}",test.proof),time_gap);
-    
-    info!("*************TEST OVER***********");
+        let time_gap =(Instant::now().duration_since(time_started).as_millis() as u32)/1000;
+        println!("testcase 942261:proof result is {},time consumed:{}s",format!("{}",test.proof),time_gap);
+    // }
 }
 
 
